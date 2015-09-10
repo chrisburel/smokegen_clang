373a711206537567f13ae08dbc4ed34fd9611a89
Chris Burel <chrisburel@gmail.com>
2015-09-10 12:12:44 -0700

    List methods that are signals and slots.

--- src/astvisitor.cpp
+++ src/astvisitor.cpp
@@ -5,5 +5,22 @@ bool SmokegenASTVisitor::VisitCXXRecordDecl(clang::CXXRecordDecl *D) {
         return false;
 
     llvm::outs() << "Visiting " << D->getQualifiedNameAsString() << "\n";
+
+    for (auto method : D->methods()) {
+        for (auto attr_it = method->specific_attr_begin<clang::AnnotateAttr>();
+          attr_it != method->specific_attr_end<clang::AnnotateAttr>();
+          ++attr_it) {
+
+            const clang::AnnotateAttr *A = *attr_it;
+
+            if (A->getAnnotation() == "qt_signal") {
+                llvm::outs() << method->getQualifiedNameAsString() << "(signal)\n";
+            }
+            else if (A->getAnnotation() == "qt_slot") {
+                llvm::outs() << method->getQualifiedNameAsString() << "(slot)\n";
+            }
+        }
+    }
+
     return true;
 }
