70031425c3abd064ca6d2c26b7904261a200142a
Chris Burel <chrisburel@gmail.com>
2015-09-11 11:15:21 -0700

    Ignore anonymous structs and unions

--- src/astvisitor.cpp
+++ src/astvisitor.cpp
@@ -1,6 +1,11 @@
 #include "astvisitor.h"
 
 bool SmokegenASTVisitor::VisitCXXRecordDecl(clang::CXXRecordDecl *D) {
+    // We can't make bindings for things that don't have names.
+    if (!D->getDeclName())
+        return false;
+
     generator.addClass(D);
+
     return true;
 }
