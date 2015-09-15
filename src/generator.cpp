0cf6e9ad66e445cf77487d9e34d8ad72d5603fb6
Chris Burel <chrisburel@gmail.com>
2015-09-15 15:15:11 -0700

    Remove useless output

--- src/generator.cpp
+++ src/generator.cpp
@@ -10,43 +10,9 @@ void SmokeGenerator::addClass(clang::CXXRecordDecl* D) {
         if (method->getAccess() == clang::AS_private)
             continue;
 
-        std::string signature("    ");
-
-        signature += method->getNameAsString() + "(";
-
-        auto end = method->param_end();
-        --end;
-        for (auto param : method->params()) {
-            auto type = param->getType();
-            signature += type.getAsString();
-            if (param != *end) {
-                signature += ", ";
-            }
-        }
-
-        signature += ")";
-
-        for (auto attr_it = method->specific_attr_begin<clang::AnnotateAttr>();
-          attr_it != method->specific_attr_end<clang::AnnotateAttr>();
-          ++attr_it) {
-
-            const clang::AnnotateAttr *A = *attr_it;
-
-            if (A->getAnnotation() == "qt_signal") {
-                signature += "(signal)";
-            }
-            else if (A->getAnnotation() == "qt_slot") {
-                signature += "(slot)";
-            }
-        }
-
         auto munged = mungedName(method);
         methodNames.insert(method->getNameAsString());
         methodNames.insert(munged);
-
-        signature += mungedName(method);
-
-        llvm::outs() << signature + "\n";
     }
 
     // Our x_* subclasses will all have destructors.  Add the destructor method
