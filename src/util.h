624022e2d1478593bd4f5a601c7535f35b15c346
Chris Burel <chrisburel@gmail.com>
2015-09-18 21:39:28 -0700

    Add isTemplate function.  Used to test if a CXXRecordDecl is a template.

--- src/util.h
+++ src/util.h
@@ -1,10 +1,15 @@
 #ifndef GENERATOR_UTIL_H
 #define GENERATOR_UTIL_H
 
+#include <clang/AST/DeclTemplate.h>
+
 template <class T>
 bool contains(const std::vector<T> &vec, const T &value)
 {
     return std::find(vec.begin(), vec.end(), value) != vec.end();
 }
 
+bool isTemplate(const clang::CXXRecordDecl *D) {
+    return (D->getDescribedClassTemplate() || clang::isa<clang::ClassTemplateSpecializationDecl>(D));
+}
 #endif
