a846b2640375473365187a014f467c948750f529
Chris Burel <chrisburel@gmail.com>
2015-09-18 20:49:17 -0700

    Write out data file shell.

--- src/generator.cpp
+++ src/generator.cpp
@@ -23,6 +23,43 @@ void SmokeGenerator::processDataStructures() {
 }
 
 void SmokeGenerator::writeDataFile(llvm::raw_ostream &out) {
+
+    // Write include lines
+    out << "#include <smoke.h>\n";
+    out << "#include <" << options->module << "_smoke.h>\n\n";
+
+    // All this stuff goes inside a namespace
+    std::string smokeNamespaceName("__smoke" + options->module);
+
+    out << "namespace " << smokeNamespaceName << " {\n\n";
+
+    out << "}\n\n"; // end namespace definition
+
+    out << "extern \"C\" {\n\n";
+
+    out << "static bool initialized = false;\n";
+    out << "Smoke *" << options->module << "_Smoke = 0;\n\n";
+    out << "// Create the Smoke instance encapsulating all the above.\n";
+    out << "void init_" << options->module << "_Smoke() {\n";
+    for (auto const &parentName : options->parentModules) {
+        out << "    init_" << parentName << "_Smoke();\n";
+    }
+    out << "    if (initialized) return;\n";
+    out << "    " << options->module << "_Smoke = new Smoke(\n";
+    out << "        \"" << options->module << "\",\n";
+    out << "        " << smokeNamespaceName << "::classes, " << /*classCount <<*/ ",\n";
+    out << "        " << smokeNamespaceName << "::methods, " << /*methodCount <<*/ ",\n";
+    out << "        " << smokeNamespaceName << "::methodMaps, " << /*methodMapCount <<*/ ",\n";
+    out << "        " << smokeNamespaceName << "::methodNames, " << /*methodNames.count() <<*/ ",\n";
+    out << "        " << smokeNamespaceName << "::types, " << /*typeIndex.count() <<*/ ",\n";
+    out << "        " << smokeNamespaceName << "::inheritanceList,\n";
+    out << "        " << smokeNamespaceName << "::argumentList,\n";
+    out << "        " << smokeNamespaceName << "::ambiguousMethodList,\n";
+    out << "        " << smokeNamespaceName << "::cast );\n";
+    out << "    initialized = true;\n";
+    out << "}\n\n";
+    out << "void delete_" << options->module << "_Smoke() { delete " << options->module << "_Smoke; }\n\n";
+    out << "}\n";
 }
 
 std::set<const clang::CXXRecordDecl *> SmokeGenerator::superClassList(const clang::CXXRecordDecl *klass) const {
