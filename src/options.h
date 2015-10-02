d4d4c8cbba43442b441848b80bbf735dad4b669f
Chris Burel <chrisburel@gmail.com>
2015-10-02 12:13:35 -0700

    Add typeMap that maps long option names to short ones used by smoke's tf_* enum.

--- src/options.h
+++ src/options.h
@@ -5,6 +5,22 @@
 #include <string>
 #include <regex>
 
+template<typename T>
+class SizeTDecider {
+};
+
+template<>
+class SizeTDecider<unsigned int> {
+public:
+    static constexpr char smokeName[] = "unit";
+};
+
+template<>
+class SizeTDecider<unsigned long> {
+public:
+    static constexpr char smokeName[] = "ulong";
+};
+
 struct FunctionMatchers {
     std::vector<std::regex> names;
     std::vector<std::regex> signatures;
@@ -20,6 +36,13 @@ struct Options {
     std::vector<std::string> voidpTypes = {"long long", "long long int", "unsigned long long", "unsigned long long int"};
     std::vector<std::string> headerList;
     std::vector<std::string> classList;
+    std::map<std::string, std::string> typeMap = {
+        {"long int", "long"},
+        {"short int", "short"},
+        {"long double", "double"},
+        {"wchar_t", "int"},
+        {"size_t", SizeTDecider<size_t>::smokeName},
+    };
     bool qtMode;
 
     std::vector<std::regex> excludeExpressions;
